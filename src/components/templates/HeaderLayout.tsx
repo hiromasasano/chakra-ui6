f5c7d0af5f40e9de8aaa10c6f739cfb7d5c5210c
hiromasasano <hsano@comsis.co.jp>
2023-10-15 13:05:16 +0900

    Initial commit

---
+++ src/components/templates/HeaderLayout.tsx
@@ -0,0 +1,17 @@
+import { memo, VFC } from "react";
+
+import { Header } from "../organism/layout/Header";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const HeaderLayout: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+});
